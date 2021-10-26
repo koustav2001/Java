b9c5bfa0a92f0735241b02814bcdeeac422af65b
koustav2001 <89209950+koustav2001@users.noreply.github.com>
2021-10-26 11:46:36 +0530

    Add files via upload

---
+++ Java Programs/Exception Handling/Sample1.java
@@ -0,0 +1,16 @@
+//Without Exception Handling
+
+import java.util.*;
+class Sample1
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the two numbers : ");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int d=a/b;
+		System.out.println("Result : "+d);
+		System.out.println("Thank You ");
+	}
+}
